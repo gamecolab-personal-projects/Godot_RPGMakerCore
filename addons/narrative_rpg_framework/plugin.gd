7be3794ce18c6851d0c1cb1018222538ea96a235
Luis Hernandez <gamecolab@outlook.com>
2025-06-20 16:43:44 +0200

    Removed automatic creation folders
    Removed automatic creation folders

--- addons/narrative_rpg_framework/plugin.gd
+++ addons/narrative_rpg_framework/plugin.gd
@@ -2,31 +2,13 @@
 extends EditorPlugin
 
 const AUTOLOAD_NAME := "GameState"
-const AUTOLOAD_PATH := "res://addons/narrative_rpg_framework/GameState.gd"
+const AUTOLOAD_PATH := "res://addons/narrative_rpg_framework/core/GameState.gd"
 
 func _enter_tree():
-	_create_game_folders()
 	_create_autoload()
 
 func _exit_tree():
 	_delete_autoload()
-
-func _create_game_folders():
-	var folders = [
-		"res://game/",
-		"res://game/core/",
-		"res://game/systems/",
-		"res://game/scenes/",
-		"res://game/data/"
-	]
-	
-	for folder_path in folders:
-		if not DirAccess.dir_exists_absolute(folder_path):
-			var err = DirAccess.make_dir_recursive_absolute(folder_path)
-			if err == OK:
-				print("Folder created:", folder_path)
-			else:
-				print("Failed to create folder:", folder_path)
 
 func _create_autoload():
 	if not ProjectSettings.has_setting("autoload/" + AUTOLOAD_NAME):
